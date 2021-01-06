7846f74552a7a770afc3d31bb4901e145f68c422
Harald Vinje <haraldvinje@gmail.com>
2021-01-06 12:52:56 +0100

    added pdf worker

--- src/pages/Resume.tsx
+++ src/pages/Resume.tsx
@@ -1,15 +1,13 @@
 import React from 'react';
+import { Document, Page } from 'react-pdf';
+import samplePDF from '../static/HaraldVinjeResume.pdf';
 
-const Resume = () => {
+const Resume: React.FC = () => {
   return (
-    <div>
-      <embed
-        src="https://s3-eu-west-1.amazonaws.com/haraldvinje.com/HaraldVinjeResume.pdf"
-        type="appliaction/pdf"
-        title="resume"
-        width="100%"
-        height="600px"
-      />
+    <div className="resume">
+      <Document file={samplePDF}>
+        <Page className="pdf-document" pageNumber={1} />
+      </Document>
     </div>
   );
 };
